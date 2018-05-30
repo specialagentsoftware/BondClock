94efca2541714951a8357597f069576215def255
jeremy bond <bond_jeremy_w@outlook.com>
2018-05-29 23:45:56 -0400

    sdalkjasdlkj

---
+++ BondClockin/BondClockIn.cs
@@ -0,0 +1,89 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.ServiceProcess;
+
+namespace BondClockin
+{
+    public partial class BondClockIn : ServiceBase
+    {
+        NancyHost host = new NancyHost(new Uri("http://localhost:8888"));
+        int SessionCheckTime = Convert.ToInt32(50);
+
+        public BondClockIn()
+        {
+            InitializeComponent();
+            this.CanHandleSessionChangeEvent = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Slackwrapper slackwrapper = new Slackwrapper();
+            slackwrapper.sendslack("Session Start " + DateTime.Now);
+
+            host.Start();
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 500;//180000;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            WebCheck check = new WebCheck();
+            try
+            {
+                check.makeCheck("https://www.trollandtoad.com");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            if (Environment.MachineName.Contains("trollandtoad.local"))
+            {
+                try
+                {
+                    check.makeCheck("https://service.trollandtoad.com");
+                }
+
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+
+            Slackwrapper slackwrapper = new Slackwrapper();
+
+            switch (changeDescription.Reason)
+            {
+                case SessionChangeReason.SessionLogon:
+                    slackwrapper.sendslack("Session Logon " + DateTime.Now + " for user " + Environment.UserName);
+                    break;
+                case SessionChangeReason.SessionLogoff:
+                    slackwrapper.sendslack("Session Logoff " + DateTime.Now + " for user " + Environment.UserName);
+                    break;
+                case SessionChangeReason.SessionLock:
+                    slackwrapper.sendslack("Session Locked " + DateTime.Now + " for user " + Environment.UserName);
+                    break;
+                case SessionChangeReason.SessionUnlock:
+                    slackwrapper.sendslack("Session Unlocked " + DateTime.Now + " for user " + Environment.UserName);
+                    break;
+            }
+
+            base.OnSessionChange(changeDescription);
+        }
+
+
+        protected override void OnStop()
+        {
+            Slackwrapper slackwrapper = new Slackwrapper();
+            slackwrapper.sendslack("Service Stop " + DateTime.Now);
+            host.Stop();
+        }
+    }
+}
---
+++ BondClockin/NancyMod.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BondClockin
+{
+    public class NancyMod : NancyModule
+
+    {
+        public NancyMod()
+        {
+            Get["/"] = _ =>
+            {
+                string ip = Request.UserHostAddress;
+                RequestHeaders headerinfo = Request.Headers;
+                return "HI, WebServer hosted by windows service here." + Environment.NewLine + "You can call me Rodarigo." + Environment.NewLine + "I see you are making the request from :: " + ip + Environment.NewLine + " :: Header as follows :: " + headerinfo.UserAgent;
+                
+            };
+        }
+    }
+}
