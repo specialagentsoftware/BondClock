bb6766ae3f431852426d2bbaed6b1adc526d5df1
Jeremy Bond <jebond@trollandtoad.com>
2018-05-31 08:40:57 -0400

    dfgdsgfdfg

--- BondClockin/NancyMod.cs
+++ BondClockin/NancyMod.cs
@@ -4,38 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
 
 namespace BondClockin
 {
     public class NancyMod : NancyModule
 
     {
-        public Dictionary<String, String> ModelArray = new Dictionary<string, string>();
-
         public NancyMod()
         {
-            Get["/dump"] = _ =>
+            Get["/"] = _ =>
             {
                 string ip = Request.UserHostAddress;
                 RequestHeaders headerinfo = Request.Headers;
                 return "HI, WebServer hosted by windows service here. <br> You can call me Rodarigo. <br> I see you are making the request from :: " + ip + "<br> :: User Agent ::<br> " + headerinfo.UserAgent;
                 
-            };
-
-            Get["/"] = _ =>
-            {
-                BondClockDataSetTableAdapters.WebStatusTableAdapter tableadapter = new BondClockDataSetTableAdapters.WebStatusTableAdapter();
-                var WebStatus = tableadapter.GetData();
-                
-                foreach (DataRow row in WebStatus)
-                {
-                    ModelArray.Add("id",row["id"].ToString());
-                    ModelArray.Add("ServerChecked",row["ServerChecked"].ToString());
-                    ModelArray.Add("TimeChecked",row["TimeChecked"].ToString());
-                    ModelArray.Add("Status",row["Status"].ToString());
-                }
-                return View["game.cshtml", ModelArray];
             };
         }
     }
