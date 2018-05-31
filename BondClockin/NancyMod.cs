9dee74ad2c545305834a26a0fb5712da290169c7
Jeremy Bond <jebond@trollandtoad.com>
2018-05-31 08:35:47 -0400

    asdfsadf

--- BondClockin/NancyMod.cs
+++ BondClockin/NancyMod.cs
@@ -19,31 +19,8 @@ namespace BondClockin
             {
                 string ip = Request.UserHostAddress;
                 RequestHeaders headerinfo = Request.Headers;
-                return "HI, WebServer hosted by windows service here. <br> You can call me Rodarigo. <br> I see you are making the request from :: " + ip + "<br>" +
-                "<ul>" +
-                "<li>Accept- " + headerinfo.Accept + "</li>" +
-                "<li>Accept Charset - " + headerinfo.AcceptCharset + "</li>" +
-                "<li>Accept Encoding - " + headerinfo.AcceptEncoding + "</li>" +
-                "<li>Accept Language - " + headerinfo.AcceptLanguage + "</li>" +
-                "<li>Authorization - " + headerinfo.Authorization + "</li>" +
-                "<li>Cache Control - " + headerinfo.CacheControl + "</li>" +
-                "<li>Connection - " + headerinfo.Connection + "</li>" +
-                "<li>Content Length - " + headerinfo.ContentLength + "</li>" +
-                "<li>Content type - " + headerinfo.ContentType + "</li>" +
-                "<li>Cookie - " + headerinfo.Cookie + "</li>" +
-                "<li>Date - " + headerinfo.Date + "</li>" +
-                "<li>Host - " + headerinfo.Host + "</li>" +
-                "<li>If Match - " + headerinfo.IfMatch + "</li>" +
-                "<li>If modified since - " + headerinfo.IfModifiedSince + "</li>" +
-                "<li>If none match - " + headerinfo.IfNoneMatch + "</li>" +
-                "<li>If range - " + headerinfo.IfRange + "</li>" +
-                "<li>If unmodified since - " + headerinfo.IfUnmodifiedSince + "</li>" +
-                "<li>Keys - " + headerinfo.Keys + "</li>" +
-                "<li>Max forwards - " + headerinfo.MaxForwards + "</li>" +
-                "<li>Referrer - " + headerinfo.Referrer + "</li>" +
-                "<li>User Agent - " + headerinfo.UserAgent + "</li>" +
-                "<li>Values- " + headerinfo.Values + "</li>" +
-                "</ul>";
+                return "HI, WebServer hosted by windows service here. <br> You can call me Rodarigo. <br> I see you are making the request from :: " + ip + "<br> :: User Agent ::<br> " + headerinfo.UserAgent;
+                
             };
 
             Get["/"] = _ =>
